66ea36a406ef88b8fea047fde67f993b8680fafb
Michael Grubb <michael.grubb@nyu.edu>
2012-06-10 15:41:57 -0400

    Initial Commit

---
+++ Jun14/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jun14
+//
+//  Created by Michael Grubb on 6/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Jun14AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Jun14AppDelegate class]));
+    }
+}
